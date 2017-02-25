0dae1f5ff6b442e3d4634c02ff340e4a7bb1347d
Hugo Gabriel <hugo.miguel.gabriel@gmail.com>
2017-02-25 15:39:42 +0000

    navigation and passing data between pages

--- Pages/EditHikePage.js
+++ Pages/EditHikePage.js
@@ -1,6 +1,4 @@
-var Observable = require("FuseJS/Observable");
-
-var hike = Observable();
+var hike = this.Parameter;
 
 var name = hike.map(x => x.name);
 var location = hike.map(x => x.location);
@@ -8,10 +6,15 @@ var distance = hike.map(x => x.distance);
 var rating = hike.map(x => x.rating);
 var comments = hike.map(x => x.comments);
 
+function goBack() {
+    router.goBack();
+}
+
 module.exports = {
     name: name,
     location: location,
     distance: distance,
     rating: rating,
-    comments: comments
+    comments: comments,
+    goBack: goBack
 };
